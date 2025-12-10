41c1145a2b3e200458021f9f1f1b3d8003fb468e
singharyan006 <aryanraisingh2006@gmail.com>
2025-12-10 22:48:33 +0530

    chore: clear main entry point for hands-on learning

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,2 @@
-#include "editor/Editor.h"
-#include <iostream>
 
-int main(int argc, char* argv[]) {
-    try {
-        Editor editor;
-        editor.init();
-        
-        if (argc >= 2) {
-            editor.openFile(argv[1]);
-        }
-        
-        editor.run();
-        
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
-    
-    return 0;
-}
 
