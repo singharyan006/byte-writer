08f97845051b151694075c017be64b92e974e477
singharyan006 <aryanraisingh2006@gmail.com>
2025-12-10 18:34:22 +0530

    chore: clear Buffer implementation for hands-on learning

--- src/utils/Buffer.cpp
+++ src/utils/Buffer.cpp
@@ -1,21 +1,2 @@
-#include "Buffer.h"
 
-Buffer::Buffer() {
-    data.reserve(1024);  // Pre-allocate for efficiency
-}
-
-Buffer::~Buffer() {
-}
-
-void Buffer::append(const std::string& s) {
-    data += s;
-}
-
-void Buffer::append(const char* s, size_t len) {
-    data.append(s, len);
-}
-
-void Buffer::clear() {
-    data.clear();
-}
 
--- src/utils/Buffer.h
+++ src/utils/Buffer.h
@@ -1,25 +1,2 @@
-#ifndef BUFFER_H
-#define BUFFER_H
 
-#include <string>
-
-// Append buffer for efficient screen rendering
-// Instead of many small write() calls, we accumulate data and write once
-class Buffer {
-public:
-    Buffer();
-    ~Buffer();
-
-    void append(const std::string& s);
-    void append(const char* s, size_t len);
-    void clear();
-    
-    const std::string& getData() const { return data; }
-    size_t getLength() const { return data.length(); }
-
-private:
-    std::string data;
-};
-
-#endif // BUFFER_H
 
